3f662b58581f128a3d7a8956a8a34d561cdfde2c
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-26 13:29:27 +0530

    EmpPresAbs

---
+++ EmpPresAbs.cs
@@ -0,0 +1,23 @@
+﻿public class Employee
+{
+	 int present = 1;
+	 void display()
+	 {
+
+		Random ran = new Random();
+		int AttenCheck = ran.Next(0, 2);
+		if (AttenCheck == present)
+		{
+			Console.WriteLine("Emp is Present");
+		}
+		else
+		{
+			Console.WriteLine("Emp is Absent");
+		}
+	 }
+	 public static void Main()
+	 {
+		Employee check = new Employee();
+		check.display();
+	 }
+}
